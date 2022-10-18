bf947984f07c94e2f9b930e5d77bcd440d890282
TerryBinful <kwakubinful@gmail.com>
2022-10-17 21:57:35 -0700

    include unistd library

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 
 /**
- *main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a new line, to the standard error 
- *Return: Always 1
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 1 (Error)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
