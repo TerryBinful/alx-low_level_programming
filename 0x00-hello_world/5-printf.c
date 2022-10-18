3e641f6167ade31f39b812e66abed9d4a6daa92f
TerryBinful <kwakubinful@gmail.com>
2022-10-17 21:29:44 -0700

    resolved betty styling issues

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+	return(0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,18 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-char c;
-int d;
-long int li;
-long long int lld;
-float f;
-
-int main (void)
+int main(void)
 {
-        printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-        printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(d));
-        printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(li));
-        printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(lld));
-        printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
-        return (0);
+	char c;
+	int d;
+	long int li;
+	long long int lld;
+	float f;
+
+        	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
+	        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+        	return(0);
 }
