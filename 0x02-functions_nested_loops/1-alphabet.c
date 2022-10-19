13ad9eae320ae51df66016c9f170fd6e20f52027
TerryBinful <kwakubinful@gmail.com>
2022-10-18 23:07:28 -0700

    Starting fresh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,1 @@
-#include "main.h"
 
-/**
- * main: print_alphabet - print alphabet
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
-}
