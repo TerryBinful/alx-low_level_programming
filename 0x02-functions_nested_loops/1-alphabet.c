6dba0b5b3a6eada2421fb8c48ed3933cb934f917
TerryBinful <kwakubinful@gmail.com>
2022-10-18 22:54:27 -0700

    first lines

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main: print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
