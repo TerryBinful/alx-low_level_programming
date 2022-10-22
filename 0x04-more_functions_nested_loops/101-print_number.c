63f64f1972e48f903f9a2e773067992ae387ce81
TerryBinful <kwakubinful@gmail.com>
2022-10-21 21:31:06 -0700

    advanced tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@
 
 void print_number(int n)
 {
-	unsigned int m, d, i;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
@@ -23,16 +23,16 @@ void print_number(int n)
 	}
 
 	d = m;
-	i = 1;
+	count = 1;
 
 	while (d > 9)
 	{
 		d /= 10;
-		i *= 10;
+		count *= 10;
 	}
 
-	for (; i >= 1; count /= 10)
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / i) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
