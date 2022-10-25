92f8e92b225336bb332f690d46a2b62fe5df4c7a
TerryBinful <kwakubinful@gmail.com>
2022-10-25 03:32:57 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string,
+ * followed by a new line
+ * @s: parameter of type pointer
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	int i, m, h;
+	char *first, *last, *temp;
+
+	i = 0;
+	m = 0;
+	h = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	h = i / 2;
+
+	while (h >= 0)
+	{
+		last = &s[i];
+		first = &s[m];
+		*temp = *last;
+		*last = *first;
+		*first = *temp;
+		i--;
+		m++;
+		h--;
+	}
+}
