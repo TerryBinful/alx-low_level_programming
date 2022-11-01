c4ffade0115e22153aa6cac214fa39f1fd440447
TerryBinful <kwakubinful@gmail.com>
2022-11-01 10:14:47 -0700

    first push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory
+ * are a pointed to  with the constant byte b
+ * @s: array
+ * @b: character to fill
+ * @n:  unsigned interger
+ * Returns: pointer to memory s
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b
+	}
+
+	return (s);
+}
