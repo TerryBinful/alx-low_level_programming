b9ae3044ae53141032af1746b7f4574a908acfc4
TerryBinful <kwakubinful@gmail.com>
2022-11-01 22:47:15 -0700

    madatory tasks

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+i#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @accept: lookup string
+ * @s: string to look in
+ * Return: length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
