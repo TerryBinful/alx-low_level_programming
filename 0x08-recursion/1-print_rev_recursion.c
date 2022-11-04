14eb3ef45ca4ff2f2aa2a5ded3a3052a0ea27c44
TerryBinful <kwakubinful@gmail.com>
2022-11-03 22:58:59 -0700

    first

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#inlude "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ * Return: none
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
