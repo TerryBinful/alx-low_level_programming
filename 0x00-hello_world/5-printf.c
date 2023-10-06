a7c928c3171165b5efbe0ba2f144c2a211fe684c
TerryBinful <kwakubinful@gmail.com>
2023-10-06 05:24:40 +0000

    all man tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - pprints exactly with proper grammar,
+* but the outcome is a piece of art
+* Return: 0 if function exits
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")p
+	return (0);
+}
+
