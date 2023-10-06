bedda161b6c4f780530d63064ae62fd206474c60
TerryBinful <kwakubinful@gmail.com>
2023-10-06 05:42:39 +0000

    made 100-intel exec and added header to 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - prints exactly and that piece of art is useful"
@@ -9,6 +10,6 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
 
