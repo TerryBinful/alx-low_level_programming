09a39ab1b553215f8eedeba81f9fd244ead014e4
TerryBinful <kwakubinful@gmail.com>
2023-10-13 03:21:19 +0000

    more man tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* add - add two integers
+* @n : n is an integer
+* @m : m is an integer
+*
+* Description: takes two integers and returns the sum
+*
+* Return: integer (sum of two integers)
+*/
+
+int add(int n, int m)
+{
+	int sum = 0;
+
+	sum = n + m;
+
+	return (sum);
+}
